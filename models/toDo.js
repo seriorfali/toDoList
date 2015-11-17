233c91c245dac13fa99f0b55fd2d15c2ca74a99b
Sean Esteva <seangeleno@gmail.com>
2015-11-17 11:08:26 -0800

    added gottaDoSchema

--- models/toDo.js
+++ models/toDo.js
@@ -1,1 +1,8 @@
 var mongoose = require('mongoose')
+
+///////////////////MONGOOSE SCHEMA//////////////////////
+var gottaDoSchema = new mongoose.Schema({
+  title: {type: String, require: true, unique: true},
+  body: String
+})
+///////////////////MONGOOSE SCHEMA/////////////////////////
